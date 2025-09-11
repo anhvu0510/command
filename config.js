c6c4397618925eb7f3e07a79b5e87b89b4dcae32
Vũ LA <2351010127minh@ou.edu.vn>
2025-09-11 13:44:06 +0700

    Add configuration for Git projects

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    gitToken: '',
+    host: 'https://git.payme.net.vn',
+    projects: [
+        {
+            projectName: 'gate-core', 
+            PID: 455,
+            mainBranch: 'main',
+            deployBranch: 'deploy'    
+        },
+        {
+            projectName: 'gate-db', 
+            PID: 456,
+            mainBranch: 'main',
+            deployBranch: 'deploy'    
+        }
+    ]
+}
